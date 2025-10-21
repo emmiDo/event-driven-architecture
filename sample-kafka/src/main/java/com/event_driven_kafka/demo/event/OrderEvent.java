5e6a32c26dbb822463b91e975584db6bf79dec95
Emmi Doherty <1767492+emmiDo@users.noreply.github.com>
2025-10-21 17:12:30 +0200

    Error fixing

--- sample-kafka/src/main/java/com/event_driven_kafka/demo/event/OrderEvent.java
+++ sample-kafka/src/main/java/com/event_driven_kafka/demo/event/OrderEvent.java
@@ -1,5 +1,14 @@
 package com.event_driven_kafka.demo.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderEvent {
 
     private Long orderId;
--- sample-kafka/src/main/java/com/event_driven_kafka/demo/event/PaymentEvent.java
+++ sample-kafka/src/main/java/com/event_driven_kafka/demo/event/PaymentEvent.java
@@ -1,7 +1,16 @@
 package com.event_driven_kafka.demo.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentEvent {
 
     private Long orderId;
